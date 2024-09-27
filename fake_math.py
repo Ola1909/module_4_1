61cf4b737eac1c489efa76acc68b88835c54bfbf
Ola1909 <etoile19@yandex.ru>
2024-09-27 15:47:36 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,8 @@
+#fake_math
+def divide(first, second):
+    if second == 0:
+       return f'Ошибка'
+    else:
+        return first / second
+
+#print(divide(3, 1))
---
+++ true_math.py
@@ -0,0 +1,14 @@
+#true_math
+#import math
+from math import inf
+
+def divide(first, second):
+    if second == 0:
+       return inf
+
+    return first / second
+
+#print(divide(15, 0))
+
+
+#ZeroDivisionError
